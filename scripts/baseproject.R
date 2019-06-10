f951bd9dff9d4de2b22553efe54e03531c97e02a
wlperry <wlperry@gmail.com>
2019-06-10 13:04:00 -0500

    intial commit

---
+++ scripts/baseproject.R
@@ -0,0 +1,10 @@
+install.packages("readxl")
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("scales")
+
+
+library(tidyverse)
+library(readxl)
+library(lubridate)
+library(scales)
